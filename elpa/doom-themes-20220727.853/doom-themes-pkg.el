1d42a97ebe5128b11b91f0d4cd30919070b75efe
hellocode777 <zhang_shuai777@163.com>
2022-08-20 07:16:01 +0800

    a

---
+++ elpa/doom-themes-20220727.853/doom-themes-pkg.el
@@ -0,0 +1,13 @@
+(define-package "doom-themes" "20220727.853" "an opinionated pack of modern color-themes"
+  '((emacs "25.1")
+    (cl-lib "0.5"))
+  :commit "aa13e7fc713d458f46f4ab7cce367fd30d801765" :authors
+  '(("Henrik Lissner" . "contact@henrik.io"))
+  :maintainer
+  '("Henrik Lissner" . "contact@henrik.io")
+  :keywords
+  '("themes" "faces")
+  :url "https://github.com/doomemacs/themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
